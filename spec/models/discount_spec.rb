9bda6333d1730b6b41b4b783684c777ac5fc6131
Ross Perry <perryr16@gmail.com>
2020-06-06 09:29:17 -0600

    relationship between merchant and discount tested

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe Merchant do 
+RSpec.describe Discount do 
 
   describe 'Relationships'do
-    it {should belong_to :merchants}
+    it {should belong_to :merchant}
   end
 
   describe 'Validations' do
-    it {should validate_presence_of :num_items}
-    it {should validate_presence_of :percent_off}
+    it {should validate_presence_of :quantity}
+    it {should validate_presence_of :percent}
   end
   
 end

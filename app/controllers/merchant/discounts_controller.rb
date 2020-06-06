372493648892f725cf2cf8a0b9552979a9831bd5
Ross Perry <perryr16@gmail.com>
2020-06-06 10:21:59 -0600

    US1 new discount built and tested

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -1,7 +1,46 @@
 class Merchant::DiscountsController < Merchant::BaseController
+
+  def index 
+    @merchant = current_user.merchant
+  end
+  
   def new
     @merchant = current_user.merchant
     @discount = @merchant.discounts.new
   end
+
+  def create
+    merchant = current_user.merchant
+    discount = merchant.discounts.create(discount_params)
+    if discount.save
+      flash[:success] = "The #{discount.name} discount has been created"
+      redirect_to merchant_discounts_path
+    else
+      flash[:error] = "Please enter a valid #{missing_params}"
+      redirect_to new_merchant_discount_path
+    end
+  end
+
+  private
+
+  def discount_params
+    name_generator
+    params.require(:discount).permit(:name, :quantity, :percent)
+  end
+  
+  def name_generator
+    params[:discount][:name] = "#{params[:discount][:percent]}% off #{params[:discount][:quantity]} items"
+  end
+
+  def missing_params
+    missing_params = []
+    params[:discount].each do |key, value|
+        missing_params << key if value == ""
+    end
+    missing_params.join(", ")
+  end
+  
+  
+  
 
 end
--- spec/features/discounts/new_spec.rb
+++ spec/features/discounts/new_spec.rb
@@ -8,10 +8,9 @@ RSpec.describe 'New Discount' do
 
   end
 
-  it "US1 merchant can create a new discount" do
+  it "US1:I merchant can create a new discount success" do
     visit new_merchant_discount_path
     expect(current_path).to eq('/merchant/discounts/new')
-    save_and_open_page
 
     within("#new-discount-form")do
       fill_in "discount[quantity]", with: 5
@@ -21,10 +20,51 @@ RSpec.describe 'New Discount' do
     end
 
     discount = Discount.last
-    expext(discount.quantity).to eq(5)
-    expext(discount.percent).to eq(50)
+    expect(discount.quantity).to eq(5)
+    expect(discount.percent).to eq(50)
+    expect(discount.name).to eq("50% off 5 items")
 
-    expect(current_path).to eq(merchant_discounts_path)    
+    expect(current_path).to eq(merchant_discounts_path) 
+    expect(page).to have_content("The #{discount.name} discount has been created")   
   end
+
+  it "US1:II merchant can't create discount if bad or missing fields" do
+
+    visit new_merchant_discount_path
+    expect(current_path).to eq('/merchant/discounts/new')
+
+    within("#new-discount-form")do
+      fill_in "discount[quantity]", with: ""
+      fill_in "discount[percent]", with: ""
+
+      click_on "Create Discount"
+    end
+    expect(current_path).to eq(new_merchant_discount_path) 
+    expect(page).to have_content("Please enter a valid quantity, percent")
+
+    expect(current_path).to eq(new_merchant_discount_path) 
+    expect(page).to have_content("Please enter a valid quantity, percent")
+
+    within("#new-discount-form")do
+      fill_in "discount[quantity]", with: 5
+      fill_in "discount[percent]", with: ""
+
+      click_on "Create Discount"
+    end
+    expect(current_path).to eq(new_merchant_discount_path) 
+    expect(page).to have_content("Please enter a valid percent")
+
+    within("#new-discount-form")do
+      fill_in "discount[quantity]", with: 5
+      fill_in "discount[percent]", with: 50
+
+      click_on "Create Discount"
+    end
+    discount = Discount.last
+    expect(current_path).to eq(merchant_discounts_path) 
+    expect(page).to have_content("The #{discount.name} discount has been created")   
+    
+  end
+  
   
 end

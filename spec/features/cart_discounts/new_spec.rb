bd2f98851d1d7b2ae3c6bf613ef99c0975cfa197
Ross Perry <perryr16@gmail.com>
2020-06-06 15:42:39 -0600

    add test for individual items exceeding discount quantity

--- spec/features/cart_discounts/new_spec.rb
+++ spec/features/cart_discounts/new_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "apply discount in cart" do
     @merchant3 = create(:merchant)
 
     @discount1 = @merchant1.discounts.create(quantity: 5, percent: 50)
-    @discount2 = @merchant1.discounts.create(quantity: 2, percent: 10)
+    @discount2 = @merchant1.discounts.create(quantity: 3, percent: 10)
     @discount3 = @merchant3.discounts.create(quantity: 1, percent: 90)
 
     @m_user = create(:user, name: "merchant user", role: 1, merchant_id: @merchant1.id)
@@ -16,6 +16,10 @@ RSpec.describe "apply discount in cart" do
 
     @item1 = create(:item, price: 10, merchant: @merchant1, inventory: 100)
     @item2 = create(:item, price: 20, merchant: @merchant2)
+    @item3 = create(:item, price: 20, merchant: @merchant1)
+    @item4 = create(:item, price: 20, merchant: @merchant1)
+    @item5 = create(:item, price: 20, merchant: @merchant1)
+    @item6 = create(:item, price: 20, merchant: @merchant1)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     
@@ -52,6 +56,28 @@ RSpec.describe "apply discount in cart" do
     expect(current_path).to eq(profile_orders_path)    
     expect(page).to have_content("Total: $45.00")
   end
+
+  it "US6 having many individual items will not trigger discount" do
+    visit item_path(@item1)
+    click_on "Add to Cart"
+    visit item_path(@item3)
+    click_on "Add to Cart"
+    visit item_path(@item4)
+    click_on "Add to Cart"
+    visit item_path(@item5)
+    click_on "Add to Cart"
+    visit item_path(@item6)
+    click_on "Add to Cart"
+
+    visit cart_path
+
+    expect(page).to have_content("Cart: 5")
+    expect(page).to_not have_content("Discount")
+    expect(page).to_not have_content("discount")
+
+    
+  end
+  
   
   
 end
